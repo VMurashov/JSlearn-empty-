4a16045f2d87f49867c7af72c75ef4a23abb839a
VMurashov <vladMurashov88@icloud.com>
2022-08-01 22:47:34 +0300

    test commit

--- js/JSlearn.js
+++ js/JSlearn.js
@@ -1,18 +1,18 @@
 let clickButton = document.querySelector(".click__button");
-let modal =document.querySelector(".modal");
+let modal = document.querySelector(".modal");
 let modalClose = document.querySelector(".modal__close");
-let modalButton =document.querySelector(".modal__button");
+let modalButton = document.querySelector(".modal__button");
 
 modal.style.display ='none';
 
-clickButton.onclick =()  =>{
-    modal.style.display='block';
+clickButton.onclick = () =>{
+    modal.style.display = 'block';
 }
 
-modalClose.onclick =() =>{
-    modal.style.display='none';
+modalClose.onclick = () =>{
+    modal.style.display = 'none';
 }
 
-modalButton.onclick =() =>{
-    modal.style.display='none';
+modalButton.onclick = () =>{
+    modal.style.display = 'none';
 }
